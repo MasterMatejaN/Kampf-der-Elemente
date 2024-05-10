3279d6d23aa56c82f39a99a824c4ddbbccb5c0cb
MasterMatejaN <154552752+MasterMatejaN@users.noreply.github.com>
2024-05-10 18:18:39 +0200

    Update main.java

--- kampfderelemente/src/main/java/com/example/kampfderelemente/main.java
+++ kampfderelemente/src/main/java/com/example/kampfderelemente/main.java
@@ -1,14 +1,20 @@
 package com.example.kampfderelemente;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class main extends Application {
 
+    private Group everything = new Group();
+    private Scene fight = new Scene(everything);
 
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setTitle("Kampf der Elemente");
+
 
 
         stage.show();
@@ -16,5 +22,9 @@ public class main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void showabilities(Stage stage) {
+
     }
 }
