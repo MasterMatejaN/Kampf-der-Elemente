6e8d7fda64a217826905dc9b2e7fe7b110584305
MasterMatejaN <154552752+MasterMatejaN@users.noreply.github.com>
2024-05-10 18:46:31 +0200

    Update main.java

--- kampfderelemente/src/main/java/com/example/kampfderelemente/main.java
+++ kampfderelemente/src/main/java/com/example/kampfderelemente/main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -12,11 +14,19 @@ import java.io.IOException;
 
 public class main extends Application {
 
+    /*Overall*/
     private Group everything = new Group();
     private Scene fight = new Scene(everything);
     public static Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
     public static int width = (int) screenBounds.getWidth();
     public static int height = (int) screenBounds.getHeight();
+
+    /*Design*/
+    private ProgressBar HpPlayer1 = new ProgressBar();
+    private ProgressBar HpPlayer2 = new ProgressBar();
+
+    private Button[] abilities1 = new Button[4];
+    private Button[] abilities2 = new Button[4];
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -34,7 +44,8 @@ public class main extends Application {
         launch(args);
     }
 
-    public void showabilities(Stage stage) {
-
+    public void showabilities() {
+        everything.getChildren().add(HpPlayer1);
+        everything.getChildren().add(HpPlayer2);
     }
 }
