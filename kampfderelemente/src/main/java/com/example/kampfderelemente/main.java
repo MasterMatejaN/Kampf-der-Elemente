fbda1440bb6afa864d1171e1c0192f683ae0f1bb
MasterMatejaN <154552752+MasterMatejaN@users.noreply.github.com>
2024-05-10 16:54:16 +0200

    Create main.java

---
+++ kampfderelemente/src/main/java/com/example/kampfderelemente/main.java
@@ -0,0 +1,20 @@
+package com.example.kampfderelemente;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
